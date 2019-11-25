705e13d052aff2b317e1cc5371d0caea37661e28
Ivan Lozano <ivanlozano@google.com>
2019-11-25 09:30:02 -0800

    Update the IHDCP,IMediaPlayerService to new binder macro.
    IMPLEMENT_META_INTERFACE is now only intended to be used by manually
    written binder interfaces, which as a practice is being deprecated. This
    macro checks the interface against a whitelist of allowed hand-written
    interfaces. Since these are generated interfaces, and thus do not need
    to be checked against the whitelist, instead call the new macro that
    skips the check, DO_NOT_DIRECTLY_USE_ME_IMPLEMENT_META_INTERFACE.
    
    Bug: 136279235
    Test: m aosp_arm64-eng
    Change-Id: I56a23c4e0b1adbffaa09fea730deb253f959b479

--- media/libmedia/IMediaPlayerService.cpp
+++ media/libmedia/IMediaPlayerService.cpp
@@ -126,7 +126,7 @@ public:
     }
 };
 
-IMPLEMENT_META_INTERFACE(MediaPlayerService, "android.media.IMediaPlayerService");
+DO_NOT_DIRECTLY_USE_ME_IMPLEMENT_META_INTERFACE(MediaPlayerService, "android.media.IMediaPlayerService");
 
 // ----------------------------------------------------------------------
 
